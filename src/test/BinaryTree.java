93b100deb6defae18e185e1d0ad6be334cba2e88
Snaiiiiil <312991025@qq.com>
2017-09-01 00:48:06 +0800

    second

--- src/test/BinaryTree.java
+++ src/test/BinaryTree.java
@@ -4,11 +4,27 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class BinaryTree {
-
 	class Node{
 		int value = 0;
 		Node lchild = null;
 		Node rchild = null;
+		Node(int value){
+			this.value = value;
+		}
+	}
+	
+	Node createTree(int[] input,int length,int i) {
+		if(input[i]!=-1 && i<length) {
+			Node node = new Node(input[i]);
+		}
+		else if(input[i] ==-1 && i<length) {
+			Node node = null;
+		}
+		createTree( input, length,++i);
+		createTree(input,length,++i);
+		
+
+		
 	}
 	
 	void preOrder(Node root,ArrayList<Node> result){
@@ -34,6 +50,11 @@ public class BinaryTree {
 		postOrder(root.rchild,result);
 		result.add(root);
 	}
+	/**
+	 * 根，然后右左结点循环入栈
+	 * @param root
+	 * @return
+	 */
 	
 	ArrayList<Node> preOrder(Node root) {
 		Stack<Node> stack = new Stack<Node>();
@@ -45,15 +66,73 @@ public class BinaryTree {
 		while(!list.isEmpty()) {
 			Node node = stack.pop();
 			list.add(node);
+			if(node.rchild!= null)
 			stack.push(node.rchild);
+			if(node.lchild!=null)
 			stack.push(node.lchild);
 		}
 		return list;
 	}
 	
+	/**
+	 * 
+	 * @param root
+	 * @return
+	 */
 	ArrayList<Node> midOrder(Node root){
 		ArrayList<Node> list = new ArrayList<Node>();
 		Stack<Node> stack = new Stack<Node>();
+		Node node = root;
+		if(root == null)
+			return list;
+		while(!stack.isEmpty() || node!=null) {
+			while(node!=null) {
+				stack.push(node);
+				node = node.lchild;
+			}
+			node = stack.pop();
+			list.add(node);
+			node = node.rchild;
+		}
+		return list;
+	}
+	/**
+	 * 刚刚打印完的结点print
+	 * 栈顶结点top
+	 * 找到top和print结点的关系
+	 * @param root
+	 * @return
+	 */
+	ArrayList<Node> postOrder(Node root){
+		ArrayList<Node> list = new ArrayList<>();
+		Stack<Node> stack = new Stack<>();
+		Node top = null;
+		Node print = root;
+		stack.push(root);
+		while(!stack.isEmpty()) {
+			top = stack.peek();
+			if(top.lchild!= null && top.lchild != print && top.rchild !=print) {
+				stack.push(top.lchild);
+				top = stack.peek();
+			}
+			else if(top.rchild!=null && top.rchild!= print) {
+				stack.push(top.rchild);
+				top = stack.peek();
+			}
+			else
+			{
+				print = stack.pop();
+				list.add(print);
+			}
+			
+		}
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		Node node = bt.new Node();
 		
 	}
+	
 }
